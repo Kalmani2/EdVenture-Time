0f11560134a0f59d30ca014b68e98df8a66c5ba5
Moeed Chughtai <moeed.chughtai@outlook.com>
2024-11-10 00:22:11 +0000

    gladiators!

--- frontend/src/components/2/RomanScene.jsx
+++ frontend/src/components/2/RomanScene.jsx
@@ -82,7 +82,10 @@ export default function MedievalScene() {
           <FloorWithEXRTexture />
           <GLBAsset filePath="/2/colosseum.glb" scale={[50, 50, 50]} position={[-4, 15, 0]} name="Gibbet" message=""/>
           <GLBAsset filePath="/2/RomanCenturion.glb" scale={[1, 1, 1]} position={[5, 2, -40]} name="Barrel" message=""/>
-          <GLBAsset filePath="/2/RomanGuy.glb" scale={[3, 3, 3]} position={[10, 2, -20]} name="Barrel" message=""/>
+          <GLBAsset filePath="/2/RomanGuy.glb" scale={[4, 4, 4]} position={[3, 1.5, -39]} name="Barrel" message=""/>
+          <GLBAsset filePath="/2/discus.glb" scale={[10, 10, 10]} position={[-40, 0, 19]} name="Barrel" message=""/>
+          <GLBAsset filePath="/2/gladiator.glb" scale={[0.2, 0.2, 0.2]} position={[-6, 0, 10]} name="Barrel" message=""/>
+          <GLBAsset filePath="/2/gladiator2.glb" scale={[0.8, 0.8, 0.8]} position={[-6, 5, -10]} name="Barrel" message=""/>
         
           {/* <AssetQuiz filePath="/1/King.glb" scale={[1, 1, 1]} position={[0, 0, 0]} jsonFile="../../1/medieval.json" name="King"/> */}
         </Suspense>
